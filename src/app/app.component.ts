c1c7a8d65aae4e261dd732447727929c78a1eec5
HonzaPrachar <jan.prrachar@gmail.com>
2020-09-09 14:26:37 +0200

    kostra prvniho dema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Formly';
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'Name',
+      type: 'input',
+      wrappers: ['form-field-horizontal'],
+      templateOptions: {
+        label: 'UserName',
+        type: 'name',
+        placeholder: 'Zadej prosim jmeno',
+        required: true,
+      },
+    },
+    {
+      key: 'Email',
+      type: 'input',
+      wrappers: ['form-field-horizontal'],
+      templateOptions: {
+        label: 'Email',
+        type: 'email',
+        placeholder: 'Zde prosim vypis email',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      wrappers: ['form-field-horizontal'],
+      templateOptions: {
+        label: 'Password',
+        type: 'password',
+        placeholder: 'heslo',
+        required: true,
+      },
+    },
+    {
+      type: 'checkbox',
+      key: 'GDPR',
+      templateOptions: {
+        label: 'Souhlasim s GDPR....',
+      },
+    },
+  ];
+  submit() {
+    alert(JSON.stringify(this.model));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { CommonModule } from '@angular/common';
+import { FormlyHorizontalWrapper } from './horizontal-wrapper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormlyHorizontalWrapper,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyBootstrapModule
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      wrappers: [{ name: 'form-field-horizontal', component: FormlyHorizontalWrapper }],
+      validationMessages: [
+        { name: 'required', message: 'Toto pole je nutne vyplnit!' },
+      ],
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
